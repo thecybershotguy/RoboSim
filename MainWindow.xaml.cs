a7ba35fe51bbe2aa0851e849d654cf536275e0c8
thecybershotguy <chauhankaranravindra@gmail.com>
2020-04-20 17:06:56 -0400

    Fixed Toolbar positioning and Borders added to the viewPort , Window now opens maximised

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,13 @@ using System.Windows.Shapes;
 
 namespace RoboSim
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+      
     }
 }
