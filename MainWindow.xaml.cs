7df96e32a922bbdbc15d8fe67e5f6512572e9372
thecybershotguy <chauhankaranravindra@gmail.com>
2020-04-20 18:32:53 -0400

    SetCamera Function added to set the view of the viewPort when initialised

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -30,6 +31,13 @@ namespace RoboSim
         public MainWindow()
         {
             InitializeComponent();
+            SetCamera();
+
+
+
+
+
+
 
             model = new Model("SwRobot");
 
@@ -38,6 +46,16 @@ namespace RoboSim
             viewPort.Children.Add(model.RobotModel);
 
 
+        }
+
+        public void SetCamera()
+        {
+
+            viewPort.RotateGesture = new MouseGesture(MouseAction.RightClick);
+            viewPort.PanGesture = new MouseGesture(MouseAction.LeftClick);
+            viewPort.Camera.LookDirection = new Vector3D(590.925,-1146.123,-427.042);
+            viewPort.Camera.UpDirection = new Vector3D(-0.248,0.481,0.841);
+            viewPort.Camera.Position = new Point3D(-590.925, 1146.123, 427.042);
         }
 
 
